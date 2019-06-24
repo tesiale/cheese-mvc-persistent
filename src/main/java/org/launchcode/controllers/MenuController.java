57a1aa76cf53186d1f3e0c01c6bf3cf40b4f9522
Tesia Le <tesiale28@gmail.com>
2019-06-24 16:58:54 -0500

    Finished Cheese MVC Persistent

---
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -0,0 +1,95 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.Cheese;
+import org.launchcode.models.Menu;
+import org.launchcode.models.data.CheeseDao;
+import org.launchcode.models.data.MenuDao;
+import org.launchcode.models.forms.AddMenuItemForm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+@RequestMapping(value="menu")
+public class MenuController {
+
+    @Autowired
+    private MenuDao menuDao;
+
+    @Autowired
+    private CheeseDao cheeseDao;
+
+    @RequestMapping(value="")
+    public String index(Model model){
+        model.addAttribute("menus", menuDao.findAll());
+        model.addAttribute("title", "Menus");
+        return "menu/index";
+    }
+
+    @RequestMapping(value="add", method = RequestMethod.GET)
+    public String add(Model model) {
+        model.addAttribute(new Menu());
+        model.addAttribute("title", "Add Menu");
+        return "menu/add";
+    }
+
+    @RequestMapping(value="add", method = RequestMethod.POST)
+    public String add(Model model,
+                      @ModelAttribute @Valid Menu menu,
+                      Errors errors){
+        if(errors.hasErrors()){
+            model.addAttribute("title", "Add Menu");
+            return "menu/add";
+        }
+
+        menuDao.save(menu);
+
+        return "redirect:view/" + menu.getId();
+    }
+
+    @RequestMapping(value="view/{id}", method = RequestMethod.GET)
+    public String viewMenu(Model model,
+                           @PathVariable int id){
+
+        Menu menu = menuDao.findOne(id);
+        model.addAttribute("menu", menu);
+        model.addAttribute("title", menu.getName());
+
+        return "menu/view";
+    }
+
+    @RequestMapping(value="add-item/{id}", method = RequestMethod.GET)
+    public String addItem(Model model,
+                           @PathVariable int id){
+
+        Menu menu = menuDao.findOne(id);
+        model.addAttribute("form", new AddMenuItemForm(menu, cheeseDao.findAll()));
+        model.addAttribute("title", "Add item to menu: " + menu.getName());
+
+        return "menu/add-item";
+    }
+
+    @RequestMapping(value="add-item", method = RequestMethod.POST)
+    public String addItem(Model model,
+                          @ModelAttribute @Valid AddMenuItemForm form,
+                          Errors errors){
+        if(errors.hasErrors()){
+            return "menu/add-item/" + form.getMenuId();
+        }
+        Menu theMenu = menuDao.findOne(form.getMenuId());
+        Cheese aCheese = cheeseDao.findOne(form.getCheeseId());
+        theMenu.addItem(aCheese);
+        menuDao.save(theMenu);
+
+        return "redirect:view/" + theMenu.getId();
+    }
+
+}
---
+++ src/main/java/org/launchcode/models/forms/AddMenuItemForm.java
@@ -0,0 +1,57 @@
+package org.launchcode.models.forms;
+
+import org.launchcode.models.Cheese;
+import org.launchcode.models.Menu;
+
+import javax.validation.constraints.NotNull;
+
+public class AddMenuItemForm {
+    private Menu menu;
+    private  Iterable<Cheese> cheeses;
+
+    @NotNull
+    private  int menuId;
+
+    @NotNull
+    private  int cheeseId;
+
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public Iterable<Cheese> getCheeses() {
+        return cheeses;
+    }
+
+    public int getMenuId() {
+        return menuId;
+    }
+
+    public int getCheeseId() {
+        return cheeseId;
+    }
+
+    public void setMenu(Menu menu) {
+        this.menu = menu;
+    }
+
+    public void setCheeses(Iterable<Cheese> cheeses) {
+        this.cheeses = cheeses;
+    }
+
+    public void setMenuId(int menuId) {
+        this.menuId = menuId;
+    }
+
+    public void setCheeseId(int cheeseId) {
+        this.cheeseId = cheeseId;
+    }
+
+    public AddMenuItemForm(){
+    }
+
+    public AddMenuItemForm(Menu menu, Iterable<Cheese> cheeses){
+        this.menu = menu;
+        this.cheeses = cheeses;
+    }
+}
